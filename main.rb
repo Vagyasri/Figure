57c29b1612c843c6b4cf7d57d61c94b3cb47765b
Lucky <vagyasri2011@gmail.com>
2021-06-21 22:40:59 +0200

    Add colors to Lucky frame

--- main.rb
+++ main.rb
@@ -1,15 +1,34 @@
+require 'colorize'
+# rubocop: disable Layout/LineLength
 
-puts " ______              ______        ______          ____________     ______       ______    ______          ______"
-puts "|      |            |      |      |      |       /             |   |      |    /      /    \     \        /     /"
-puts "|      |            |      |      |      |      /     _________/   |      |   /      /      \     \      /     /"
-puts "|      |            |      |      |      |     |     /             |      |  /      /        \     \    /     /"
-puts "|      |            |      |      |      |     |    |              |      | /      /          \     \  /     /"
-puts "|      |            |      |      |      |     |    |              |      |/      /            \     \/     /"
-puts "|      |            |      |      |      |     |    |              |             /              \          /"
-puts "|      |            |      |      |      |     |    |              |      |\     \               \        /"
-puts "|      |            |      |      |      |     |    |              |      | \     \               |      |"
-puts "|      |            |      |      |      |     |    |              |      |  \     \              |      |"
-puts "|      |            |      |      |      |     |    |              |      |   \     \             |      |"
-puts "|       \________   |       \____/       |     |     \__________   |      |    \     \            |      |"
-puts "|                |   \                  /       \               |  |      |     \     \           |      |"
-puts "|_______________/     \________________/         \_____________/   |______/      \_____\          |______|"
+puts ' ______              ______        ______          ____________     ______       ______    ______          ______'.colorize(:red)
+sleep(1)
+puts '|      |            |      |      |      |       /             |   |      |    /      /    \\     \\        /     /'.colorize(:red)
+sleep(1)
+puts '|      |            |      |      |      |      /     _________/   |      |   /      /      \\     \\      /     /'.colorize(:red)
+sleep(1)
+puts '|      |            |      |      |      |     |     /             |      |  /      /        \\     \\    /     /'.colorize(:red)
+sleep(1)
+puts '|      |            |      |      |      |     |    |              |      | /      /          \\     \\  /     /'.colorize(:red)
+sleep(1)
+puts '|      |            |      |      |      |     |    |              |      |/      /            \\     \\/     /'.colorize(:blue)
+sleep(1)
+puts '|      |            |      |      |      |     |    |              |             /              \\          /'.colorize(:blue)
+sleep(1)
+puts '|      |            |      |      |      |     |    |              |      |\\     \\               \\        /'.colorize(:blue)
+sleep(1)
+puts '|      |            |      |      |      |     |    |              |      | \\     \\               |      |'.colorize(:blue)
+sleep(1)
+puts '|      |            |      |      |      |     |    |              |      |  \\     \\              |      |'.colorize(:green)
+sleep(1)
+puts '|      |            |      |      |      |     |    |              |      |   \\     \\             |      |'.colorize(:green)
+sleep(1)
+puts '|       \\_______    |       \\____/       |     |     \\________     |      |    \\     \\            |      |'.colorize(:green)
+sleep(1)
+puts '|               |    \\                  /       \\             |    |      |     \\     \\           |      |'.colorize(:green)
+sleep(1)
+puts '|_______________/     \\________________/         \\___________/     |______/      \\_____\\          |______|'.colorize(:green)
+
+puts ''
+
+# rubocop: enable Layout/LineLength
